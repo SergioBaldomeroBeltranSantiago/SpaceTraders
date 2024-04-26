15b493c8e4cdd13dd82fcc3d2b9f2ab2aeba039f
Sergio Baldomero Beltran Santiago <sergiobaldomero.beltransantiago@gmail.com>
2024-04-25 20:28:00 -0700

    Retrabajado CSS y simplificado

--- assets/js/switchMenuOption.js
+++ assets/js/switchMenuOption.js
@@ -1,12 +1,12 @@
 function switchMenuOption(evt, selectedOption) {
 	var index, optionsList, optionsButton;
 
-	optionsList = document.getElementsByClassName("menuContent");
+	optionsList = document.getElementsByClassName("spaceSubMenu");
 	for (index = 0; index < optionsList.length; index++) {
 		optionsList[index].style.display = "none";
 	}
 
-	optionsButton = document.getElementsByClassName("menuOptions");
+	optionsButton = document.getElementsByClassName("spaceMenuButton");
 	for (index = 0; index < optionsButton.length; index++) {
 		optionsButton[index].className = optionsButton[index].className.replace(
 			" active",
