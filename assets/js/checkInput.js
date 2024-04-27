f916c5d947d96ca0d80a395711ec6cb088c9a0fc
Sergio Baldomero Beltran Santiago <sergiobaldomero.beltransantiago@gmail.com>
2024-04-26 20:13:51 -0700

    Añadido script para verificar el input de Token valido

--- assets/js/checkInput.js
+++ assets/js/checkInput.js
@@ -2,6 +2,10 @@ function checkInputs() {
 	var fileInput = document.getElementById("spaceTokenInputFile");
 	var textInput = document.getElementById("spaceTokenInputText");
 	var findAgent = document.getElementById("spaceFindAgent");
+	var clearFileInput = document.getElementById("spaceClearInputFile");
+
+	clearFileInput.style.display =
+		fileInput.files.length > 0 ? "block" : "none";
 
 	findAgent.style.display =
 		fileInput.files.length > 0 || textInput.value.trim() !== ""
