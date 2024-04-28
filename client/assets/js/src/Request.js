765bed053c62de952ceef5fb8d32c90d33f717b3
Brian Caldera <brian_alex07@outlook.com>
2024-04-28 15:39:21 -0400

    Added method to Requests class to hit endpoints that require agent token

--- client/assets/js/src/Request.js
+++ client/assets/js/src/Request.js
@@ -3,11 +3,13 @@
  * Use this class to initiate any request.
  */
 export default class Requests {
+
+    #BASE_URL = 'https://api.spacetraders.io/v2'
     
     constructor() {}
 
-    /** Send request to endpoint with body as argument
-     * @param {string} endpoint the API endpoint to hit. Example: '/my/agent'
+    /** Send a request to the endpoint with body as argument
+     * @param {string} endpoint the API endpoint to hit.
      * @param {Object} body the request object
      * @param {string} [method='POST'] the method to use. Default is POST
      * @returns {Promise} a promises that can be resolved. Use then() as usual
@@ -22,9 +24,33 @@ export default class Requests {
             body: JSON.stringify(body)
         }
         
-        const URL = `https://api.spacetraders.io/v2${endpoint}`
+        const URL = this.#BASE_URL + endpoint
         console.log(options);
         // send request to endpoint
+        return fetch(URL, options)
+    }
+
+    /** Send a request with token to the endpoint with body as argument.
+     * Use this method to hit endpoints that require the authorization token.
+     * @param {String} token the authorization token for the agent
+     * @param {string} endpoint the API endpoint to hit. Example: '/my/agent'
+     * @param {Object} body the request object
+     * @param {string} [method='POST'] the method to use. Default is POST
+     * @returns {Promise} a promises that can be resolved. Use then() as usual
+     * */
+    sendAuth(token, endpoint, body, method = 'POST') {
+
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(body)
+        }
+
+        const URL = this.#BASE_URL + endpoint
+
         return fetch(URL, options)
     }
 }
