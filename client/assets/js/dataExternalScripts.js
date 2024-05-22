88c22fb870bed817eb5eef66deb83e71b151ab4a
Sergio Baldomero Beltran Santiago <sergiobaldomero.beltransantiago@gmail.com>
2024-05-21 19:10:02 -0700

    Format

--- client/assets/js/dataExternalScripts.js
+++ client/assets/js/dataExternalScripts.js
@@ -6,32 +6,30 @@ controlar diferentes acciones
 */
 
 function validateToken() {
-	var tokenID = document.getElementById("spaceTokenInputText").value;
+  var tokenID = document.getElementById("spaceTokenInputText").value;
 
-	if (tokenID !== null && tokenID !== "") {
-		const payload = {
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: "Bearer " + tokenID,
-			},
-		};
+  if (tokenID !== null && tokenID !== "") {
+    const payload = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + tokenID,
+      },
+    };
 
-		fetch("https://api.spacetraders.io/v2/my/agent", payload)
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error(
-						"Token not found. Status:" + response.status,
-					);
-				}
-				response.json();
-			})
-			.then((response) => {
-				document.getElementById("tokenHolder").innerText = tokenID;
-				alert("Token found, redirecting to menu");
-			})
-			.catch((error) => {
-				console.error(error);
-				alert("Token was not found, please try again");
-			});
-	}
+    fetch("https://api.spacetraders.io/v2/my/agent", payload)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Token not found. Status:" + response.status);
+        }
+        response.json();
+      })
+      .then((response) => {
+        document.getElementById("tokenHolder").innerText = tokenID;
+        alert("Token found, redirecting to menu");
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Token was not found, please try again");
+      });
+  }
 }
--- client/assets/js/visualScripts.js
+++ client/assets/js/visualScripts.js
@@ -3,14 +3,14 @@ JavaScript, injecta componentes a la pagina, algo similar a como
 ReactJS o angular manejar los componentes modularizados
 */
 function fetchHTML(templatePath, containerId) {
-	fetch(templatePath)
-		.then((response) => response.text())
-		.then((data) => {
-			document.getElementById(containerId).innerHTML = data;
-		})
-		.catch((error) => {
-			console.error("Error fetching template for " + divId, error);
-		});
+  fetch(templatePath)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(containerId).innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error fetching template for " + divId, error);
+    });
 }
 
 /*
@@ -19,23 +19,23 @@ hace click en uno de los botones del menu
 */
 
 function switchMenuOption(evt, selectedOption) {
-	var index, optionsList, optionsButton;
+  var index, optionsList, optionsButton;
 
-	optionsList = document.getElementsByClassName("spaceSubMenu");
-	for (index = 0; index < optionsList.length; index++) {
-		optionsList[index].style.display = "none";
-	}
+  optionsList = document.getElementsByClassName("spaceSubMenu");
+  for (index = 0; index < optionsList.length; index++) {
+    optionsList[index].style.display = "none";
+  }
 
-	optionsButton = document.getElementsByClassName("spaceMenuButton");
-	for (index = 0; index < optionsButton.length; index++) {
-		optionsButton[index].className = optionsButton[index].className.replace(
-			" active",
-			"",
-		);
-	}
+  optionsButton = document.getElementsByClassName("spaceMenuButton");
+  for (index = 0; index < optionsButton.length; index++) {
+    optionsButton[index].className = optionsButton[index].className.replace(
+      " active",
+      ""
+    );
+  }
 
-	document.getElementById(selectedOption).style.display = "block";
-	evt.currentTarget.className += " active";
+  document.getElementById(selectedOption).style.display = "block";
+  evt.currentTarget.className += " active";
 }
 
 /*
@@ -43,8 +43,8 @@ JavaScript, una funcion que chequea el valor de los campos donde el usuario
 puede incluir su token de agente para acceder a su progreso de juego.
 */
 function checkInputs() {
-	var textInput = document.getElementById("spaceTokenInputText");
-	var findAgent = document.getElementById("spaceFindAgent");
+  var textInput = document.getElementById("spaceTokenInputText");
+  var findAgent = document.getElementById("spaceFindAgent");
 
-	findAgent.style.display = textInput.value.trim() !== "" ? "block" : "none";
+  findAgent.style.display = textInput.value.trim() !== "" ? "block" : "none";
 }
